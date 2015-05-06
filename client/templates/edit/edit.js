116251876adaaf800c18c092a771a0123ba4ae88
mangster <magnus.westrom@gmail.com>
2015-05-06 19:29:23 +0200

    Fixed dragdrop sort bug

--- client/templates/edit/edit.js
+++ client/templates/edit/edit.js
@@ -32,16 +32,13 @@ Template.edit.onRendered(function () {
             // Only make the .panel-heading child elements support dragging.
             // Omit this to make then entire <li>...</li> draggable.
             handle: '.panel-heading', 
+			
             update: function() {
                 $('.panel', panelList).each(function(index, elem) {
-					
-                     var $listItem = $(elem),
-                         newIndex = $listItem.index();
-
+					 var $listItem = $(elem),
+                     newIndex = $listItem.index();
                      // Persist the new indices.
-					 console.log(this.id);
-					 console.log(newIndex);
-					 Whos.update({_id: this.id}, {$set: {rank: newIndex}});
+					 Whos.update({_id: this.id}, {$set: {rank: index}});
                 });
             }
 	});
